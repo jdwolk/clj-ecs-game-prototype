a165afdcfd7bb8011a3034dc0fe2da77000b37bf
Jordaniel <jwolk@uci.edu>
2012-12-25 23:36:47 -0800

    adding project.clj and README

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject ecs-test "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :local-repo "lib"
+  :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [midje "1.4.0"]
+                 [com.stuartsierra/lazytest "1.2.3"]]
+  :repositories {"stuart" "http://stuartsierra.com/maven2"})
