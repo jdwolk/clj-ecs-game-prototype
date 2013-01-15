358f58ca26fe2f6d8d77d62adc0b85e4b247035d
Jordaniel <jwolk@uci.edu>
2013-01-15 10:40:07 -0800

    Multiple moving NPCs onscreen

--- src/ecs_test/main.clj
+++ src/ecs_test/main.clj
@@ -13,7 +13,38 @@
   (:gen-class
     :main main))
 
+;TODO Need a better way to handle config settings
+(def screen-width 350)
+(def screen-height 350)
+
+;TODO move this somewhere else!!!
+;XXX THIS IS HORRID TEST CODE
+(def npcs (ref {}))
+
+;XXX Don't like this...
+; its a horrible hack
+(defn assoc-npc-in-pool [npc]
+  (if npc
+    (do
+      ;(println "***** Associating NPC " (get-ent-id npc))
+      (alter npcs assoc (get-ent-id npc) npc))))
+
 (set! *warn-on-reflection* true)
+
+(defn make-body [x y dir & {vis :visual :or
+                           {vis :player_down}}]
+  (make-entity (make-comp Position x y 0)
+               (make-comp Direction dir)
+               (make-comp Velocity 0)
+               (make-comp Visual vis)))
+
+(defn make-npc []
+  (make-entity (make-comp Position (rand-int screen-width) 
+                                   (rand-int screen-height) 0)
+               (rand-direction)
+               (rand-velocity 5)
+               (make-comp Visual :player_up)))
+               ;(make-comp Behavior :random-movement)))
 
 (def first-entity 
   (ref (make-entity (make-comp Position 10 300 0)
@@ -21,45 +52,46 @@
                     (make-comp Velocity 0)
                     (make-comp Visual :player_down))))
 
-(def npc-entity
-  (ref (make-entity (make-comp Position 200 200 0)
-                    (make-comp Direction :E)
-                    (make-comp Velocity 0)
-                    (make-comp Visual :player_right))))
-                    ;(make-comp Behavior :random-walking))))
+(defn draw-entity [#^SunGraphics2D g ent]
+  (let [pos (get-comp ent :Position)
+        img (lookup-img (get-comp ent :Visual))]
+    ;(println "Drawing " (get-ent-id ent))
+    ;(println "Position: " pos)
+    ;(println "Vis: " img "\n\n")
+    (draw g (image-shape (:x pos) (:y pos) img)
+          (style :background (color 224 0 0 128)))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;XXX horrible, horrible. I'm going to hell.
+(defn rand-move [npc]
+  ;(println "Moving NPC " (get-ent-id npc))
+  (if (= (rand-int 15) 0) ; should move?
+      (let [{npc-pos :Position npc-vis :Visual} 
+            (apply-compfn something npc)]
+      (assoc-comp (assoc-comp npc npc-pos) npc-vis))))
+
+(def move-agent (agent nil))
+
+(defn mover [x]
+  (send-off *agent* #'mover)
+  (doseq [npc (vals @npcs)] 
+    (let [moved-npc (rand-move npc)]
+      ;(println "NPC " (get-ent-id npc) " moved")
+      (dosync
+        (assoc-npc-in-pool moved-npc))))
+  (. Thread (sleep 100)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   
 (defn paint-world [#^JPanel c #^SunGraphics2D g]
   (dosync
-    (if (= (rand-int 15) 0) ; should move?
-      (let [{npc-pos :Position npc-vis :Visual} 
-            (apply-compfn something @npc-entity)]
-        (alter npc-entity (fn [e] (assoc-comp e npc-pos)))
-        (alter npc-entity (fn [e] (assoc-comp e npc-vis)))))
-    
     (let [an-ent (alter first-entity
                          (fn [e] (assoc-comp e (apply-compfn delta-loc e))))
           new-ent (alter first-entity
                          (fn [e] (assoc-comp e (apply-compfn direction-img e))))
-          new-pos (get-comp @first-entity :Position)
-          new-vis (get-comp @first-entity :Visual)
-          new-img (lookup-img new-vis)
-          npc-pos (get-comp @npc-entity :Position)
-          npc-vis (get-comp @npc-entity :Visual)]
-    (push g
-      (draw g (image-shape (:x new-pos) (:y new-pos) new-img)
-              (style :background (color 224 0 0 128)))
-      (draw g (image-shape (:x npc-pos) (:y npc-pos) (lookup-img npc-vis))
-              (style :background (color 224 0 0 128)))))))
-
-      ;(draw g (image-shape (:x (get-comp @npc-entity :Position))
-      ;                     (:y (get-comp @npc-entity :Position))
-      ;                     (lookup-img (get-comp @npc-entity :Visual)))
-      ;        (style :background (color 224 0 0 128)))))))
-
-
-;(draw g (circle 150 150 30)
-;        (style :background (color 110 10 10))))))
-     
+          all-ents (cons new-ent (vals @npcs))]
+        (doseq [e all-ents]
+          (push g
+            (draw-entity g e))))))
      
 ;TODO carryover fns from initial prototype
 (defn key-dispatch [#^KeyEvent e]
@@ -78,13 +110,13 @@
 
 (def screen (canvas :id :gamescreen
                     :paint paint-world
-                    :size [350 :by 350]))
+                    :size [screen-width :by screen-height]))
 
 (defn setup-frame [screen]
   (println "Setting up frame!")
   (let [t (timer (fn [e] (repaint! screen)) :delay 60)
         f (frame :title "My Game"
-                 :size [350 :by 350]
+                 :size [screen-width :by screen-height]
                  :content screen
                  :on-close :dispose)]
     (native!)
@@ -94,5 +126,9 @@
 
 (defn -main []
   (println "in -main")
+  (dosync
+    (dotimes [_ 8]
+      (assoc-npc-in-pool (make-npc))))
+  (send-off move-agent mover)
   (setup-frame screen))
 
